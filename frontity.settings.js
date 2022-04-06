44650e1ec15538ddaec7cbcae2b4ab5e352d0b0b
Angga Arkademi <angga.arkademi@gmail.com>
2022-04-06 10:25:10 +0700

    initial release

---
+++ frontity.settings.js
@@ -0,0 +1,73 @@
+const settings = {
+  "name": "tia",
+  "state": {
+    "frontity": {
+      "url": "https://id.techinasia.com",
+      "title": "Test Frontity Blog",
+      "description": "Wordpress API Data"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Premium",
+              "/category/premium-content/"
+            ],
+            [
+              "Partnership",
+              "/category/partnership-content/"
+            ],
+            [
+              "Update",
+              "/category/startup/"
+            ],
+            [
+              "Teknologi",
+              "/category/teknologi/"
+            ],
+            [
+              "Rangkuman",
+              "/category/brief/"
+            ],
+            [
+              "Executif Brief",
+              "/category/executive-brief/"
+            ],
+            [
+              "Agenda",
+              "/category/event/"
+            ],
+            [
+              "Data",
+              "/category/data/"
+            ]
+          ],
+          "featured": {
+            "showOnList": true,
+            "showOnPost": true
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "url": "https://id.techinasia.com"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
